e31e5970faf491b100bd1dea226af33d4c3d9002
Charryl Subrio <charrylsubrio@gmail.com>
2023-11-21 21:29:58 +0800

    Update incomeCalculatorAnalyzer1.c
    Added basic functions of the options.

--- incomeCalculatorAnalyzer1.c
+++ incomeCalculatorAnalyzer1.c
@@ -1,35 +1,94 @@
 /*
- * This is a comprehensive source code for our final project in CCS102.
-
- * INCOME CALCULATOR/ANALYZER - This program provides a user-friendly interface for calculating and analyzing daily, weekly, monthly, and yearly income.
-       Users can select the desired time frame and input the necessary information, and the program will automatically calculate and display the 
-	   total amount, average, and other relevant statistics. This valuable data can help users track their income patterns, identify trends, and make 
-	   informed financial decisions. 
-
- * LOMARDA, Ismael Marlon
- * SUBRIO, Charryl
- * TORAJA, Herro
-
- * Date: November 11, 2023
- */
-
+Description: 
+//Programmers: 
+LOMARDA, ISMAEL MARLON
+SUBRIO, CHARRYL
+TORAJA, HERRO
+//Date finished:
+*/
 
 #include <stdio.h>
 
-int main() {
+int main(){
+	char option;
+	int period, income, tIncome = 0, sum;
+	float average;
 	
-	int choice; 
+	do{
 	
-	printf("INCOME CALCULATOR/ANALYZER\n\n");
+		do{
+			//this will display the menu and validates the choice of the user
+			printf("** MENU **\n\n");
+			printf("[D] - Daily\n");
+			printf("[W] - Weekly\n");
+			printf("[M] - Monthly\n");
+			printf("[Y] - Yearly\n");
+			printf("[Q] - Quit\n");
+			
+			printf("\nPress option... ");
+			option = getche();
+			
+			if(toupper(option) != 'D' && toupper(option) != 'W' && toupper(option) != 'M' && toupper(option) != 'Y' && toupper(option) != 'Q') {
+				printf("\n\nInvalid option, Please refer to the menu above.\n\n\n");
+			}
+		} while(toupper(option) != 'D' && toupper(option) != 'W' && toupper(option) != 'M' && toupper(option) != 'Y' && toupper(option) != 'Q');
 	
-	printf("1. Calculate Daily Income\n");
-	printf("2. Calculate Weekly Income\n");
-	printf("3. Calculate Monthly Income\n");
-	printf("4. Calculate Yearly Income\n");
-	printf("5. Exit\n");
-	
-	printf("\nInput your choice: ");
-	scanf("%d", &choice);
+		switch(toupper(option)) {
+		case 'D': 
+			printf("\n\nNumber of Days: ");
+			scanf("%d", &period);
+			printf("\n");
+			for(int i = 1; i <= period; i++) {
+				printf("Income Day %d: ", i);
+				scanf("%d", &income);
+				tIncome += income; 
+			}
+			printf("\nTotal income within %d Day(s): %d\n\n", period, tIncome);
+			average = tIncome / period;
+			printf("Average Daily Income: %.2f\n\n\n", average);
+			break;
+		case 'W':
+			printf("\n\nNumber of Weeks: ");
+			scanf("%d", &period);
+			printf("\n");
+			for(int i = 1; i <= period; i++) {
+				printf("Income Week %d: ", i);
+				scanf("%d", &income);
+				tIncome += income; 
+			}
+			printf("\nTotal income within %d Week(s): %d\n\n", period, tIncome);
+			average = tIncome / period;
+			printf("Average Weekly Income: %.2f\n\n\n", average);
+			break;
+		case 'M':
+			printf("\n\nNumber of Months: ");
+			scanf("%d", &period);
+			printf("\n");
+			for(int i = 1; i <= period; i++) {
+				printf("Income Month %d: ", i);
+				scanf("%d", &income);
+				tIncome += income; 
+			}
+			printf("\nTotal income within %d Month(s): %d\n\n", period, tIncome);
+			average = tIncome / period;
+			printf("Average Monthly Income: %.2f\n\n\n", average);
+			break;
+		case 'Y':
+			printf("\n\nNumber of Years: ");
+			scanf("%d", &period);
+			printf("\n");
+			for(int i = 1; i <= period; i++) {
+				printf("Income Year %d: ", i);
+				scanf("%d", &income);
+				tIncome += income; 
+			}
+			printf("\nTotal income within %d Year(s): %d\n\n", period, tIncome);
+			average = tIncome / period;
+			printf("Average Yearly Income: %.2f\n\n\n", average);
+			break;
+    	}
+    	
+	} while(toupper(option) != 'Q'); 
 
 	return 0;
 }
